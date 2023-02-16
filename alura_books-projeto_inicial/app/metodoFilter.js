199d90309fd2255469e811f7a3c9b5fcdaadd778
Fernando Carlos <111884680+Solarck@users.noreply.github.com>
2023-02-16 20:42:20 -0300

    ultima aula

--- alura_books-projeto_inicial/app/metodoFilter.js
+++ alura_books-projeto_inicial/app/metodoFilter.js
@@ -10,6 +10,29 @@ function filtrarLivros() {
     // Isso fara com que ao clicar nos botoes possam ser verificados seu valor(value), no caso a categoria
     // para que possa ser feita a filtragem.
     const categoria = elementoBtn.value
-    let livrosFiltrados = categoria == 'disponivel' ? livros.filter(livro => livro.quantidade > 0) : livros.filter(livro => livro.categoria == categoria)
+    // O codigo anterior foi extraiodo para uma função global
+    // Isso melhora a legibilidade do codigo
+    let livrosFiltrados = categoria == 'disponivel' ? filtrarPorDispobilidade() : filtrarPorCategoria(categoria)
     exibirOsLivros(livrosFiltrados)
+
+    if (categoria == 'disponivel') {
+        const valorTotal = calcularValorTotalDosDisponiveis(livrosFiltrados)
+        exibirValorTotal(valorTotal)
+    }
+
+    function exibirValorTotal(valorTotal) {
+        valoTotalLivrosDispo.innerHTML = ` <div class="livros__disponiveis">
+        <p>Todos os livros disponíveis por R$ <span id="valor">${valorTotal}</span></p>
+        </div>`
+    }
+}
+
+// Função de escopo global
+function filtrarPorCategoria(categoria) {
+    return livros.filter(livro => livro.categoria == categoria)
+}
+
+// Função de escopo global
+function filtrarPorDispobilidade() {
+    return livros.filter(livro => livro.quantidade > 0)
 }
--- alura_books-projeto_inicial/app/metodoForEach.js
+++ alura_books-projeto_inicial/app/metodoForEach.js
@@ -1,15 +1,15 @@
 const InserirLivros = document.getElementById('livros')
-
+const valoTotalLivrosDispo = document.getElementById('valor_total_livros_disponiveis')
 function exibirOsLivros(listaDeLivros) {
   InserirLivros.innerHTML = ''
+  valoTotalLivrosDispo.innerHTML=''
   listaDeLivros.forEach(livro => {
 
     // let disponibilidade = verificaDisponibilidadeLivro(livro) 
     // Como podemos ver a baixo a nossa  verificação pode ser reduzida de uma função para uma linha.
     let disponibilidade = livro.quantidade > 0 ? 'livro_imagens' : 'livros_imagens indisponivel'
     // A linha a cima pode ser explicada como "livro.quantidade é maior que 0 " se for ("?") 
-    // retorne 'livro_imagens' caso contrario retorne '
-    console.log(disponibilidade)
+    // retorne 'livro_imagens' caso contrario retorne 'livros_imagens indisponivel'
 
     InserirLivros.innerHTML += `
         <div class="livro">
